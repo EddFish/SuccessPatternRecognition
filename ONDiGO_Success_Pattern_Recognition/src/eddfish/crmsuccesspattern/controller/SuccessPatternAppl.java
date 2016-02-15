c149fc5640ab8128dc70aac43377a206b4b25a7d
edward <edward@EdwardLFish>
2016-02-15 11:47:07 +0200

    Add density of communications, and mode of this.

--- ONDiGO_Success_Pattern_Recognition/src/eddfish/crmsuccesspattern/controller/SuccessPatternAppl.java
+++ ONDiGO_Success_Pattern_Recognition/src/eddfish/crmsuccesspattern/controller/SuccessPatternAppl.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.web.client.RestTemplate;
 
 import eddfish.crmsuccesspattern.dao.ONDiGO;
 import eddfish.crmsuccesspattern.model.PatternOfCRM;
@@ -13,29 +14,36 @@ import eddfish.crmsuccesspattern.model.PatternOfCRM;
 public class SuccessPatternAppl {
 
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+
 		/*
 		 * RestTemplate rest = new RestTemplate(); ONDiGO[] crm =
-		 * rest.getForObject("", ONDiGO[].class);
+		 * rest.getForObject("http://localhost:63342/Hello/crm.html",
+		 * ONDiGO[].class);
 		 */
+
 		ObjectMapper mapper = new ObjectMapper();
 		ONDiGO[] crm = mapper.readValue(new File("opportunities.json"), ONDiGO[].class);
-		/*
-		 * for (int i = 0; i < crm.length; i++) { Stages[] stg =
-		 * crm[i].getStages(); for (int j = 0; j < stg.length; j++) {
-		 * System.out.print(stg[j].getName() + " ");
-		 * System.out.println(stg[j].getDate()); } }
-		 */
+
 		try {
 			PatternOfCRM pattern = new PatternOfCRM(crm, "yyyy-MM-dd'T'HH:mm:ss+hh:mm", 86400000);
-			System.out.println("Life Time: "+pattern.getLifeTimeEVandSD());
-			double[] modeLT=pattern.getModeOfLifeTime();
+			System.out.println("Life Time: " + pattern.getLifeTimeEVandSD());
+			double[] modeLT = pattern.getModeOfLifeTime();
+			System.out.println("Mode of Life Time:");
 			for (int i = 0; i < modeLT.length; i++) {
-				System.out.println(modeLT[i]);
+				System.out.print(modeLT[i] + ", ");
 			}
-			System.out.println("Number of Communications: "+pattern.getnCommunicationsEVanSD());
-			double[] modeNC=pattern.getModenCommunications();
+			System.out.println();
+			System.out.println("Number of Communications: " + pattern.getnCommunicationsEVanSD());
+			double[] modeNC = pattern.getModenCommunications();
+			System.out.println("Mode of communications number:");
 			for (int i = 0; i < modeNC.length; i++) {
-				System.out.println(modeNC[i]);
+				System.out.print(modeNC[i] + ", ");
+			}
+			System.out.println();
+			double[] modeDensity = pattern.getModeOfDensity();
+			System.out.println("Mode of communications density:");
+			for (int i = 0; i < modeDensity.length; i++) {
+				System.out.print(modeDensity[i] + ", ");
 			}
 
 		} catch (Exception e) {
--- ONDiGO_Success_Pattern_Recognition/src/eddfish/crmsuccesspattern/model/PatternOfCRM.java
+++ ONDiGO_Success_Pattern_Recognition/src/eddfish/crmsuccesspattern/model/PatternOfCRM.java
@@ -2,10 +2,12 @@ package eddfish.crmsuccesspattern.model;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.math3.util.Precision;
 
 import eddfish.crmsuccesspattern.dao.EVandSD;
 import eddfish.crmsuccesspattern.dao.ONDiGO;
@@ -19,18 +21,20 @@ public class PatternOfCRM {
 	EVandSD nCommunicationsEVanSD;
 	double[] modeOfLifeTime;
 	double[] modenCommunications;
+	double[] modeOfDensity;
 
-	public PatternOfCRM(ONDiGO[] crm, String df, long period) throws ParseException  {
+	public PatternOfCRM(ONDiGO[] crm, String df, long period) throws ParseException {
 		nOfWons = stateCounting(crm, "won");
 		this.df = new SimpleDateFormat(df);
 		this.mlSecByPeriod = period;
-		double[] ltArray=creatArrayOfLifeTime(crm, "won");
-		double[] ncArray=creatArrayOfCommunications(crm, "won");
-		this.modeOfLifeTime=StatUtils.mode(ltArray);
+		double[] ltArray = creatArrayOfLifeTime(crm, "won");
+		double[] ncArray = creatArrayOfCommunications(crm, "won");
+		double[] densArray = createArrayOfDensityCommunications(ltArray, ncArray);
+		this.modeOfLifeTime = StatUtils.mode(ltArray);
 		this.modenCommunications = StatUtils.mode(ncArray);
-		this.lifeTimeEVandSD = calcEVandSD(ltArray,modeOfLifeTime.length);
-		this.nCommunicationsEVanSD = calcEVandSD(ncArray,modenCommunications.length);
-		
+		this.modeOfDensity = StatUtils.mode(densArray);
+		this.lifeTimeEVandSD = calcEVandSD(ltArray, modeOfLifeTime.length);
+		this.nCommunicationsEVanSD = calcEVandSD(ncArray, modenCommunications.length);
 
 	}
 
@@ -40,6 +44,10 @@ public class PatternOfCRM {
 
 	public double[] getModeOfLifeTime() {
 		return modeOfLifeTime;
+	}
+
+	public double[] getModeOfDensity() {
+		return modeOfDensity;
 	}
 
 	public EVandSD getLifeTimeEVandSD() {
@@ -52,16 +60,14 @@ public class PatternOfCRM {
 
 	private EVandSD calcEVandSD(double[] arr, int modeLength) {
 		double ev, sd;
-		if(modeLength==1 && arr.length>30){
+		if (modeLength == 1 && arr.length > 30) {
 			ev = StatUtils.mean(arr);
-			StandardDeviation objSD = new StandardDeviation();
-			sd = objSD.evaluate(arr, ev);
-		}
-		else {
+			sd = FastMath.sqrt(StatUtils.variance(arr));
+		} else {
 			ev = StatUtils.percentile(arr, 50);
-			sd = (StatUtils.percentile(arr, 75)-StatUtils.percentile(arr, 25))/2;
+			sd = (StatUtils.percentile(arr, 75) - StatUtils.percentile(arr, 25)) / 2;
 		}
-		
+
 		return new EVandSD(ev, sd);
 
 	}
@@ -75,6 +81,7 @@ public class PatternOfCRM {
 				k++;
 			}
 		}
+		// Arrays.sort(res);
 		return res;
 	}
 
@@ -87,7 +94,7 @@ public class PatternOfCRM {
 				long min = getDateByString(tmpStage[0].getDate());
 				long max = min;
 				for (int j = 1; j < tmpStage.length; j++) {
-					long currentDate=getDateByString(tmpStage[j].getDate());
+					long currentDate = getDateByString(tmpStage[j].getDate());
 					max = FastMath.max(max, currentDate);
 					min = FastMath.min(min, currentDate);
 				}
@@ -95,11 +102,23 @@ public class PatternOfCRM {
 				k++;
 			}
 		}
+		// Arrays.sort(res);
+		return res;
+	}
+
+	private double[] createArrayOfDensityCommunications(double[] ltArray, double[] ncArray) {
+		double[] res = new double[nOfWons];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = ncArray[i] / ltArray[i];
+			res[i] = Precision.round(res[i], 2);
+		}
 		return res;
 	}
 
 	private double getTimeInterval(long begin, long end) {
-		return ((double)(end - begin)) / mlSecByPeriod;
+		double res = ((double) (end - begin)) / mlSecByPeriod;
+		res = Precision.round(res, 2);
+		return res;
 	}
 
 	private long getDateByString(String date) throws ParseException {
